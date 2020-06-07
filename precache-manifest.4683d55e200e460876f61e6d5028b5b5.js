032f0501a79096d9739f465eb83f317ce66c8252
saikiran.velankayala <saikiran.velankayala@servicenow.com>
2020-06-07 22:42:58 +0530

    Updates

---
+++ precache-manifest.4683d55e200e460876f61e6d5028b5b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5af9aa6ba959d67283882b2afda7a4",
+    "url": "/voice-and-keyboard-snake-game/index.html"
+  },
+  {
+    "revision": "998573d98a8fe0f687e0",
+    "url": "/voice-and-keyboard-snake-game/static/css/main.7dd0a022.chunk.css"
+  },
+  {
+    "revision": "7413c3f6b9deda51efa8",
+    "url": "/voice-and-keyboard-snake-game/static/js/2.f44eaaa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voice-and-keyboard-snake-game/static/js/2.f44eaaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998573d98a8fe0f687e0",
+    "url": "/voice-and-keyboard-snake-game/static/js/main.cea98092.chunk.js"
+  },
+  {
+    "revision": "a15fca616a0451d57f46",
+    "url": "/voice-and-keyboard-snake-game/static/js/runtime-main.2b8bb054.js"
+  },
+  {
+    "revision": "0178e5fdddabadbd77d691c73263a424",
+    "url": "/voice-and-keyboard-snake-game/static/media/pacman-black.0178e5fd.png"
+  }
+]);
